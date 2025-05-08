31c2852f2a05436e7a73325ff6bb9e7188da8acb
Aldair Zamora <aldair.zamora@outlook.com>
2025-05-08 12:17:43 -0600

    initial commit

---
+++ TextPilot/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TextPilot
+//
+//  Created by Aldair Zamora on 5/8/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ TextPilotTests/TextPilotTests.swift
@@ -0,0 +1,17 @@
+//
+//  TextPilotTests.swift
+//  TextPilotTests
+//
+//  Created by Aldair Zamora on 5/8/25.
+//
+
+import Testing
+@testable import TextPilot
+
+struct TextPilotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
